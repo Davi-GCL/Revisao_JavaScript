c99d767ff28b6258af912231a1b847a9b73b9178
Davi-Lemos <128177405+Davi-Lemos@users.noreply.github.com>
2023-03-23 15:33:14 -0300

    add: 10arrays.js , 11objeto.js

---
+++ 10arrays.js
@@ -0,0 +1,78 @@
+//Topico avancado 1:
+
+    // function intersec(arr1, arr2){
+    //     let arrAux = [];
+    //     for(i = 0; i <= arr1.length; i++){
+    //         arr2.includes(arr1[i]) ? arrAux.push(arr1[i]) : null;
+    //     }
+
+    //     return arrAux;
+    // }
+
+    // console.log(intersec([0,1,2,4,10],[1,2,3,4,5,6,7,8,9,10]));
+
+
+//Topico avancado 3:
+
+    // function maior18(arrObj){
+    //     let arrAux = [];
+    //     for(let i =0; i < arrObj.length; i++){
+    //         arrObj[i].idade > 18 ? arrAux.push(arrObj[i]) : null;
+    //     }
+
+    //     return arrAux;
+    // }
+
+    // let pessoas = [
+    //     {
+    //         nome: "Jorge",
+    //         idade: 40
+    //     },
+    //     {
+    //         nome: "Leonardo",
+    //         idade: 17
+    //     },
+    //     {
+    //         nome: "Enzo",
+    //         idade: 12
+    //     },
+    //     {
+    //         nome: "Ana",
+    //         idade: 22
+    //     }
+    // ];
+
+    // console.log(maior18(pessoas));
+
+
+//Topico avancado 9:
+
+    // function novoArray(arrObj){
+    //     let arrAux = [];
+    //     for(let i =0; i < arrObj.length; i++){
+    //         arrObj[i].idade > 18 && arrObj[i].nome.includes("a") ? arrAux.push(arrObj[i]) : null;
+    //     }
+
+    //     return arrAux;
+    // }
+
+    // let pessoas = [
+    //     {
+    //         nome: "jorge",
+    //         idade: 40
+    //     },
+    //     {
+    //         nome: "leonardo",
+    //         idade: 17
+    //     },
+    //     {
+    //         nome: "enzo",
+    //         idade: 12
+    //     },
+    //     {
+    //         nome: "ana",
+    //         idade: 22
+    //     }
+    // ];
+
+    // console.log(novoArray(pessoas));
